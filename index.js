4d140f5a21742142b527c848ffadd9a1b741d19b
Joseph Lee <64094466+jooo-lee@users.noreply.github.com>
2022-02-02 01:15:06 +0000

    Send daily message at specified time

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const Discord = require("discord.js");
 const fetch = (...args) => import("node-fetch").then(({default: fetch}) => fetch(...args));
 const keepAlive = require("./server");
+const cron = require("node-cron");
 
 // Create a new client instance
 const client = new Discord.Client({
@@ -31,7 +32,7 @@ client.on("messageCreate", async (msg) => {
 
   // Just for fun hehe
   if (msg.content === "hi") {
-    msg.reply("no.");
+    msg.reply(":fortune_cookie:");
   }
 
   // Send quote of the day when someone types "$dailyquote"
@@ -45,3 +46,27 @@ keepAlive();
 
 // Log in to Discord with client's token
 client.login(process.env['TOKEN']);
+
+/* 
+Note to self:
+Replit uses UTC timezone, so when dealing with time you need
+to convert it to whatever timezone you're currently in
+*/
+cron.schedule("0 12 * * *", async function() { // convert UTC to EST, this is 7am EST 
+  client.channels.cache.get("936761148244627478").send("It is 7am. Have a nice day!");
+  const quote = await getQuote();
+  client.channels.cache.get("936761148244627478").send(quote);
+});
+
+/* 
+TODO:
+- weather api
+  - general weather
+  - temp
+  - humidity
+  - expected max UV index
+- word of the day
+- song of the day
+  - watch rest of FCC discord.js vid
+  - can i make a queue? w replit db
+*/
